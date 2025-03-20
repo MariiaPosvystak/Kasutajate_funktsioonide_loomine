5dbd859bcf4b6bfd83b70ffa24f50e0e571ea77e
opilane <opilane@HP-CZC2349HT4>
2025-03-20 14:52:48 +0200

    wref

--- Kasutajate_funktsioonide_loomine.py
+++ Kasutajate_funktsioonide_loomine.py
@@ -1,24 +1,24 @@
 from module1_def import *
 
-# #arithmetic funktsiooni kasutamine
-# a=float(input("Sisesta arv 1: "))
-# b=float(input("Sisesta arv 2: "))
-# t=input("Tehe: ")
-# v=arithmetic(a,b,t)
-# print(v)
+#arithmetic funktsiooni kasutamine
+a=float(input("Sisesta arv 1: "))
+b=float(input("Sisesta arv 2: "))
+t=input("Tehe: ")
+v=arithmetic(a,b,t)
+print(v)
 
-# #is_year_leap funktsiooni kasutamine
-# aasta=int(input("Mis aasta tahad kontrollida? "))
-# v=is_year_leap(aasta)
-# print(v)
-# if v:
-#     print(f"{aasta} on liigaasta")
-# else:
-#     print(f"{aasta} ei ole liigaasta")
+#is_year_leap funktsiooni kasutamine
+aasta=int(input("Mis aasta tahad kontrollida? "))
+v=is_year_leap(aasta)
+print(v)
+if v:
+    print(f"{aasta} on liigaasta")
+else:
+    print(f"{aasta} ei ole liigaasta")
 
-# #square funktsiooni kasutamine
-# S,P,d=square(float(input("Sisesta külg: ")))
-# print(S,P,d)
+#square funktsiooni kasutamine
+S,P,d=square(float(input("Sisesta külg: ")))
+print(S,P,d)
 
 #season funktsiooni kasutamine
 v=is_prime(int(input("Sisesta arv: ")))
